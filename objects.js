7f6f48a64299e241c15498c4e6a5ebb988554e4f
ADIL NAIMI <adilnm23@hotmail.com>
2019-01-13 05:19:44 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist={"Michael Jackson": "man in the mirror "}
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign(obj, { key:value})
+  return Object.assign(playlist, { artistName:songTitle})
 }
