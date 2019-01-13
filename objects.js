2311c752de73efc505439762e4a8e2b3335e576b
ADIL NAIMI <adilnm23@hotmail.com>
2019-01-13 05:38:57 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist={"Michael Jackson": "man in the mirror "}
+var playlist={Michael: "man in the mirror "}
 function updatePlaylist(playlist, artistName, songTitle){
   playlist= Object.assign(playlist, { artistName:songTitle})
   return playlist
@@ -8,5 +8,5 @@ function removeFromPlaylist(obj, artistName){
 delete obj.artistName
 return obj
 }
-removeFromPlaylist(playlist, "Michael Jackson")
+delete playlist.Michael
 console.log(playlist)
