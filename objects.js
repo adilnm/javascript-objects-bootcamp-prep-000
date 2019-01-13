fd88a8f94411cc0b55f1e46002d143fb24f3dc30
ADIL NAIMI <adilnm23@hotmail.com>
2019-01-13 05:26:08 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ function updatePlaylist(playlist, artistName, songTitle){
   return playlist
 }
 
-function updatePlaylist(playlist, artistName, songTitle){
-  playlist= Object.assign(playlist, { artistName:songTitle})
-  return playlist
+function removeFromPlaylist(playlist, artistName){
+delete playlist.artistName
+return playlist
 }
